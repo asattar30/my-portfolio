4ce94bab9c5236babb5190e30d3e748e1d0f42ae
Ali Sattar <alisattarbarani@outlook.com>
2020-10-13 22:04:30 +0300

    Updates

---
+++ precache-manifest.a42b83c04f25ba9737de756e5e0191a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9274b1dac7fe69276426b7cebd29ed04",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "2dc1bed74c8fb28ed882",
+    "url": "/my-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "266ea96734122670004e",
+    "url": "/my-portfolio/static/css/main.91f115dd.chunk.css"
+  },
+  {
+    "revision": "2dc1bed74c8fb28ed882",
+    "url": "/my-portfolio/static/js/2.238690fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-portfolio/static/js/2.238690fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266ea96734122670004e",
+    "url": "/my-portfolio/static/js/main.effe8da8.chunk.js"
+  },
+  {
+    "revision": "a561e9fe35ca2092c890",
+    "url": "/my-portfolio/static/js/runtime-main.55518df0.js"
+  }
+]);
